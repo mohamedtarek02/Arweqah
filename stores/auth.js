0e28ee83caa832f221495ba105fa054c78432556
motarek-dev <mtarek@futureface.sa>
2025-06-18 17:25:23 +0300

    Remove store logic

--- stores/auth.js
+++ stores/auth.js
@@ -3,23 +3,14 @@ import { useStorage } from "@vueuse/core";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
-    user: useStorage("auth", {}),
+
   }),
 
   getters: {
-    isAuthenticated: (state) => !!state.user?.Token,
-    firstUserLetters: (state) => {
-      if (!state.user || !state.user.FullName) return "";
-      return state.user.FullName.split(" ")
-        .map((n) => n[0])
-        .join("")
-        .toUpperCase();
-    },
+   
   },
 
   actions: {
-    updateUser(currentUser) {
-      this.user = currentUser;
-    },
+
   },
 });
