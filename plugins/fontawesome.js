a5a913bad34ff0f2483a93c00a27e525b97df87d
motarek-dev <mtarek@futureface.sa>
2025-06-18 13:30:02 +0300

    use free fonawesome instead of pro one

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fal } from "@fortawesome/pro-light-svg-icons";
-import { far } from "@fortawesome/pro-regular-svg-icons";
+import { fal } from "@fortawesome/free-light-svg-icons"; 
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 config.autoAddCss = false;
 
